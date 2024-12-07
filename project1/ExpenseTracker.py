c2c76731ac71bc5c99b8a55c5510855fc2e4ab9b
Cy <yuec03071@gmail.com>
2024-12-07 04:54:16 +0000

    update

---
+++ project1/ExpenseTracker.py
@@ -0,0 +1,75 @@
+print("""
+Personal Expense Tracker
+      1.Add Transaction
+      2.Edit Transaction
+      3.Delect Transaction
+      4.View Summary
+      5.Save and Exit
+""")
+choice = input("Enter your choice: ")
+while choice != "5":
+    if choice == "1":
+        print("Add TRansaction")
+
+
+        print("""
+Personal Expense Tracker
+      1.Add Transaction
+      2.Edit Transaction
+      3.Delect Transaction
+      4.View Summary
+      5.Save and Exit
+""")
+        choice = input("Enter your choice: ")
+    elif choice == "2":
+          print("Edit Transaction")
+          print("""
+Personal Expense Tracker
+      1.Add Transaction
+      2.Edit Transaction
+      3.Delect Transaction
+      4.View Summary
+      5.Save and Exit
+""")
+          choice = input("Enter your choice: ")
+    elif choice == "3":
+          print("Delect Transaction")
+          print("""
+Personal Expense Tracker
+      1.Add Transaction
+      2.Edit Transaction
+      3.Delect Transaction
+      4.View Summary
+      5.Save and Exit
+""")
+          choice = input("Enter your choice: ")
+    elif choice == "4":
+          print("View Summary")
+          print("""
+Personal Expense Tracker
+      1.Add Transaction
+      2.Edit Transaction
+      3.Delect Transaction
+      4.View Summary
+      5.Save and Exit
+""")
+          choice = input("Enter your choice: ")
+else:
+     print("Save and exit")
+
+
+import pandas as pd
+df = pd.DataFrame()
+
+
+
+
+
+
+     
+
+
+
+        
+        
+      
